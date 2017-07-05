c594108b44339cd15c3f7eaf0883b5ebd6611895
amandamc4 <amanda.mc4@hotmail.com>
2017-07-05 13:12:36 -0400

    until31

--- routes/app.js
+++ routes/app.js
@@ -1,40 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
-// router.get('/', function (req, res, next) {
-//     res.render('index');
-// });
-
-// router.get('/message/:msg', function (req, res, next) {
-//     res.render('node', {message: req.params.msg});
-// });
-
-router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-        if (err){
-            return res.send('Error!');
-        }
-        // render the response inside the callback because findOne is async
-        res.render('node', {email: doc.email});
-    });
-});
-
-// router.post('/message', function (req, res, next) {
-//     var message = req.body.message;
-//     res.redirect('/message/' + message);
-// });
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Amanda',
-        lastName: 'Marques',
-        password: 'super-secret',
-        email: email
-    }); 
-    user.save();
-    res.redirect('/');
+//only needs to render index so Angular2 takes over routing
+router.get('/', function (req, res, next) {    
+   res.render('index');
 });
 
 module.exports = router;
