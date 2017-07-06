599098887822e6fe21b069a1141e0252af678e10
amandamc4 <amanda.mc4@hotmail.com>
2017-07-06 15:21:08 -0400

    addingNewMessages

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { Message } from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Amanda'),
-        new Message('Another message', 'Amanda'),
-    ];
+export class AppComponent {   
 }
