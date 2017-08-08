8f746aebe1a034b9b7637350598b8c3ee3e61b70
amandamc4 <amanda.mc4@hotmail.com>
2017-08-08 15:14:08 -0400

    adding messages

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,7 @@ var Message = require('../models/message');
 
 router.post('/', function (req, res, next) {    
   var message = new Message({
-        content: req.body.content;
+        content: req.body.content
   });
   message.save(function(err, result){
       if(err){
@@ -19,6 +19,22 @@ router.post('/', function (req, res, next) {
           obj: result
       });
   });
+});
+
+router.get('/', function(req, res, next){
+    Message.find()
+        .exec(function(err, messages){
+            if(err){
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            });
+        });
 });
 
 module.exports = router;
