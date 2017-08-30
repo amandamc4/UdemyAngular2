c6342d47bd8308cf7f016b3044de67d9f225604c
amandamc4 <amanda.mc4@hotmail.com>
2017-08-30 14:16:22 -0400

    wrap up user with message

--- routes/messages.js
+++ routes/messages.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var User = require('../models/user');
 
 var Message = require('../models/message');
 
 router.get('/', function(req, res, next){
     Message.find()
+        .populate('user', 'firstName')
         .exec(function(err, messages){
             if(err){
                 return res.status(500).json({
@@ -32,25 +34,38 @@ router.use('/', function(req, res, next){
     })
 });
 
-router.post('/', function (req, res, next) {    
-  var message = new Message({
-        content: req.body.content
-  });
-  message.save(function(err, result){
-      if(err){
+router.post('/', function (req, res, next) {   
+  var decoded = jwt.decode(req.query.token);   
+  User.findById(decoded.user._id, function (err, user){
+    if(err){
         return res.status(500).json({
             title: 'An error occured',
             error: err
         });
-      }
-      res.status(201).json({
-          message: 'Saved',
-          obj: result
-      });
-  });
+    }  
+    var message = new Message({
+        content: req.body.content,
+        user: user
+    });
+    message.save(function(err, result){
+        if(err){
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        user.messages.push(result);
+        user.save();
+        res.status(201).json({
+            message: 'Saved',
+            obj: result
+        });
+    });
+  }); 
 });
 
 router.patch('/:id', function(req, res, next){
+    var decoded = jwt.decode(req.query.token);  
     Message.findById(req.params.id, function(err, message){
         if(err){
             return res.status(500).json({
@@ -62,6 +77,12 @@ router.patch('/:id', function(req, res, next){
             return res.status(500).json({
                 title: 'No message found',
                 error: {message: 'Message not found'}
+            });
+        }
+        if(message.user != decoded.user._id){
+            return res.status(401).json({
+                title: 'Not authenticated',
+                error: {message: 'Users do not match'}
             });
         }
         message.content = req.body.content;
@@ -81,6 +102,7 @@ router.patch('/:id', function(req, res, next){
 });
 
 router.delete('/:id', function(req, res, next){
+    var decoded = jwt.decode(req.query.token);  
     Message.findById(req.params.id, function(err, message){
         if(err){
             return res.status(500).json({
@@ -94,7 +116,12 @@ router.delete('/:id', function(req, res, next){
                 error: {message: 'Message not found'}
             });
         }
-        
+        if(message.user != decoded.user._id){
+            return res.status(401).json({
+                title: 'Not authenticated',
+                error: {message: 'Users do not match'}
+            });
+        }
         message.remove(function(err, result){
             if(err){
                 return res.status(500).json({
