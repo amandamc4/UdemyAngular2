2370f25e15651f9aae9233947fa27623c02c861d
amandamc4 <amanda.mc4@hotmail.com>
2017-09-01 12:37:38 -0400

    AoT and modules

--- assets/app/errors/error.service.ts
+++ assets/app/errors/error.service.ts
@@ -7,4 +7,5 @@ export class ErrorService {
     handleError(error: any){
         const errorData = new Error(error.title, error.error.message);
         this.errorOccurred.emit(errorData);
+    }
 }
